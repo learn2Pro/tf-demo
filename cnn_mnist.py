8932ad306bcd05f52024fab8d7e0521b4295620a
tang <tyqw9110@qq.com>
2017-02-09 10:32:58 +0800

    1.add minist cnn and softmax

---
+++ cnn_mnist.py
@@ -0,0 +1,84 @@
+import tensorflow as tf
+from tensorflow.contrib.learn.python.learn.datasets.mnist import read_data_sets
+
+mnist = read_data_sets("./MNIST_data/", one_hot=True)
+sess = tf.InteractiveSession()
+
+
+# weight noise
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+
+# bias noise
+def bias_variable(shape):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
+
+# 卷积
+def conv2d(x, W):
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+
+# max pooling
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+                          strides=[1, 2, 2, 1], padding='SAME')
+
+
+# 初始化占位，变量
+x = tf.placeholder("float", shape=[None, 784])
+y_ = tf.placeholder("float", shape=[None, 10])
+
+# 5x5的patch中算出32个特征,两个维度是patch的大小，接着是输入的通道数目(只有一个通道0,1)，最后是输出的通道数目
+W_conv1 = weight_variable([5, 5, 1, 32])
+b_conv1 = bias_variable([32])
+# 把x变成一个4d向量，其第2、第3维对应图片的宽、高，
+# 最后一维代表图片的颜色通道数(因为是灰度图所以这里的通道数为1，
+# 如果是rgb彩色图，则为3)
+x_image = tf.reshape(x, [-1, 28, 28, 1])
+# 第一层卷积和池化 ReLU激活函数
+h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+# 32@12*12 f maps
+h_pool1 = max_pool_2x2(h_conv1)
+# 第二层卷积 第二层中，每个5x5的patch会得到64个特征
+W_conv2 = weight_variable([5, 5, 32, 64])
+b_conv2 = bias_variable([64])
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+# 64@4*4 f maps
+h_pool2 = max_pool_2x2(h_conv2)
+
+W_fc1 = weight_variable([7 * 7 * 64, 1024])
+b_fc1 = bias_variable([1024])
+
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
+#full con
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+# dropout 防止过拟合
+keep_prob = tf.placeholder("float")
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+W_fc2 = weight_variable([1024, 10])
+b_fc2 = bias_variable([10])
+
+y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+
+cross_entropy = -tf.reduce_sum(y_ * tf.log(y_conv))  # 交叉熵
+train_step = tf.train.GradientDescentOptimizer(1e-4).minimize(cross_entropy)
+
+correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+sess.run(tf.initialize_all_variables())
+for i in range(1000):
+    batch = mnist.train.next_batch(50)
+    if i % 100 == 0:
+        train_accuracy = accuracy.eval(feed_dict={
+            x: batch[0], y_: batch[1], keep_prob: 1.0})
+        print("step %d, training accuracy %g" % (i, train_accuracy))
+    train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+
+print("test accuracy %g" % accuracy.eval(feed_dict={
+    x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
---
+++ multi.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+
+input1 = tf.constant(3.0)
+input2 = tf.constant(2.0)
+input3 = tf.constant(5.0)
+intermed = tf.add(input2, input3)
+mul = tf.mul(input1, intermed)
+
+
+with tf.Session() as sess:
+  with tf.device("/gpu:0"):
+    result = sess.run([mul, intermed])
+    print(result)
