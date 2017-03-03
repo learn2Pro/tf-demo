001b3ca8e6628390d5614f04f7f67d7ba2d80c8a
tang <tyqw9110@qq.com>
2017-03-03 18:30:06 +0800

    1.add bpnn algorithm

--- multi.py
+++ multi.py
@@ -4,7 +4,7 @@ input1 = tf.constant(3.0)
 input2 = tf.constant(2.0)
 input3 = tf.constant(5.0)
 intermed = tf.add(input2, input3)
-mul = tf.mul(input1, intermed)
+mul = tf.multiply(input1, intermed)
 
 
 with tf.Session() as sess:
